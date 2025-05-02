bc83bb87668e598f6cf334f987cf76853ca11e49
Nicholas <njg4ne@virginia.edu>
2025-05-01 20:32:06 -0400

    publish to docs

--- bundled/vite.config.js
+++ bundled/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: '/nasa-tlx-input/',
     build: {
-        outDir: 'docs',
+        outDir: '../docs',
         // target: 'esnext', // Ensures support for top-level await
         // minify: 'esbuild', // Enables minification using esbuild (default)
     },
